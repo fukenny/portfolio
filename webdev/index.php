f8f026cb89dadbfaf5fb0f1f9d1f2377cfd88ea5
Kenneth Solans <ken@aqdct.com>
2014-04-23 18:07:36 -0500

    Updated Webdev content

--- webdev/index.php
+++ webdev/index.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Web Development - Kenneth Solans</title>
-		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="shortcut icon" href="../favicon.png">
 		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 		<link rel="stylesheet" href="../css/themes/default/jquery.mobile-1.4.2.min.css">
 		<link rel="stylesheet" href="../_assets/css/jqm-kms.css">
@@ -27,48 +27,74 @@
 			<div role="main" class="ui-content jqm-content">
 				<h1>It All Starts With the Internet</h1>
 				<p>Not only is the internet not going away, it is growing. According to research by <a href="http://www.pewresearch.org/about/" target="_blank">the Pew Research Center</a>, 87% of adults in the United States use the internet on a daily basis. With a number like that, I can't think of a reason why a business shouldn't have an online presence.</p>
-				<h2>Website Development</h2>
+				
+				<h2>How ksolans.com Was Built</h2>
+				<div class="jqm-block-content-clear">
+					<p>ksolans.com was built using jQuery Mobile libraries without the use of a CMS. It is clean, lightweight, and completely responsive. I've added Google Analytics to the site to track traffic. Here was my process in building ksolans.com.</p>
+					
+					<p>I started by defining what pages were needed. The homepage is a given. But I decided to make that more of an "About Me" page instead of an overview of the site. Since Android and web development are different, I knew I needed a page for each. I considered adding social media in the web development page, but decided there would be enough content for it to have it's own page. I understand people do not want to spend ten minutes on a pages reading large blocks of text. Having a page for my resume was the main reason for creating the site, so it received it's own page. A contact page is necessary on just about every website, so for now, that would be my final page.</p>					
+				</div>
 				<div class="ui-grid-a ui-responsive">
 					<div class="ui-block-a">
 						<div class="jqm-block-content-clear">
-							<p>My approach to web development is simple. Find the message you want the user to get. That means forget all the bells and whistles and focus on your primary message and build off of that. That doesn't mean a site can't be unique, but I believe a clean sleek responsive design that only gives the information needed is best for the user. From there we can decide the best way to bring that message across.</p>
-							<p>ksolans.com was built using jQuery Mobile libraries without the use of a CMS. It is clean, lightweight, and completely responsive. I've added Google Analytics to the site to track traffic. You can find all the source code for this site on my <a href="https://github.com/fukenny/portfolio" target="_blank">Github account</a>.</p>
+							<p>Next I needed to figure out how I wanted to build the site. I could have used Wordpress or a framework like Foundation or Bootstrap. But I had been reading about jQuery Mobile and liked the amount of freedom it would give me with the site, especially making it responsive. Also, since it was for me, I didn't need to worry about making the backend user friendly. This is the first site I have created using the jQuery Mobile libraries.</p>
 						</div>
 					</div>
 					<div class="ui-block-b">
 						<div class="jqm-block-content">
-							<img style="width: 100%" src="../_assets/img/aqdct-site.png">
+							<img style="width: 100%" src="../_assets/img/jqm.jpg">
 						</div>
 					</div>
 				</div>
-				<h2>Content Management</h2>
-				<div class="jqm-block-content-clear">
-					<p>Content is key. This is what is going to drive people to your site and bring them back again and again. Setting up a system where adding additional content that is simple and user friendly is not the easiest of tasks. Luckily the development of Content Management Systems (CMS) helps take a lot of the pain out of the process.</p>
-					<p>I've worked with a global company managing websites in over 15 countries using Joomla! and Liferay. Within that company I worked along side the editorial team to obtain the content to post and the design team to make sure each new page added fits with the companies brand standards. I also worked with the companies Director of Web Strategies to make updates to each of the sites within the company around the globe. With this experience I learned how to communicate effectively across each team I needed to work with and how to multitask and prioritize assignments. This communication became especially important while I was working on websites where the content was not in english.</p>
+					<div style="clear: both;" class="jqm-block-content-clear">
+						<p>Coming up with the content for each page was possibly the hardest part for me. I felt the best thing to do with the content was to give it a voice. I want the reader to feel like they can hear me telling them a story as they read what is on the page. After I came up my content I built out the framework of the site and started adjusting the CSS to give it the feel that I wanted. I needed the site to convey that I am professional, but also have some personality.</p>
+						<p>Throughout the process I was backing up the site with git, and pushing to my repository on Github. I made sure to add Google Analytics to each page and pointed the domain to the server. I was able to get to this point in about 20 hours from conception to going live. But that wasn't the end of the process. Next came testing. Did it work on every browser? Did all my links point to the correct place? Are my images all displaying correctly? Where any pages taking too long to load? How does it look on my phone? After I was able to answer all these questions the way I wanted to, I then considered it live.</p>
+						<p>You can find all the source code for this site on my <a href="https://github.com/fukenny/portfolio" target="_blank">Github account</a>.</p>
+					</div>
+					<h2>Website Development</h2>
+					<div class="ui-grid-a ui-responsive">
+						<div class="ui-block-a">
+							<div class="jqm-block-content-clear">
+								<p>My approach to web development is simple. Find the message you want the user to get. That means forget all the bells and whistles and focus on your primary message and build off of that. That doesn't mean a site can't be unique, but I believe a clean sleek responsive design that only gives the information needed is best for the user. From there we can decide the best way to bring that message across.</p>
+							</div>
+						</div>
+						<div class="ui-block-b">
+							<div class="jqm-block-content">
+								<img style="width: 100%" src="../_assets/img/aqdct-site.png">
+							</div>
+						</div>
+						<div style="clear: both;" class="jqm-block-content-clear">
+							<p>I also believe a website is never "finished". Going live is just a milestone. There is no point to a website that doesn't update itself with new content and new features. In certain cases it can even be dangerous and leave you vulnearable to being hacked.</p>
+						</div>
+					</div>
+					<h2>Content Management</h2>
+					<div class="jqm-block-content-clear">
+						<p>Content is key. This is what is going to drive people to your site and bring them back again and again. Setting up a system where adding additional content that is simple and user friendly is not the easiest of tasks. Luckily the development of Content Management Systems (CMS) helps take a lot of the pain out of the process.</p>
+						<p>I've worked with a global company managing websites in over 15 countries using Joomla! and Liferay. Within that company I worked along side the editorial team to obtain the content to post and the design team to make sure each new page added fits with the companies brand standards. I also worked with the companies Director of Web Strategies to make updates to each of the sites within the company around the globe. With this experience I learned how to communicate effectively across each team I needed to work with and how to multitask and prioritize assignments. This communication became especially important while I was working on websites where the content was not in english.</p>
+					</div>
+					<h2>Blogs</h2>
+					<div class="jqm-block-content-clear">
+						<p>Sometimes a company doesn't need a full website, but has lots of content to share. Or maybe there is a website, but the company wants to show off their ideas and convey personality. I've used Wordpress to set up blogs for clients to manage, as well as run a blog to help promote a product.</p>
+					</div>
+					<a href="#ui-page-top" class="jqm-deeplink ui-icon-carat-u ui-alt-icon">Top</a>
 				</div>
-				<h2>Blogs</h2>
-				<div class="jqm-block-content-clear">
-					<p>Sometimes a company doesn't need a full website, but has lots of content to share. Or maybe there is a website, but the company wants to show off their ideas and convey personality. I've used Wordpress to set up blogs for clients to manage, as well as run a blog to help promote a product.</p>
+				<!-- /main content -->
+				<!-- Menu -->
+				<div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
+					<ul class="jqm-list ui-alt-icon ui-nodisc-icon">
+						<li data-filtertext="KMS Ken Kenneth Solans home homepage" data-icon="home" class="ui-first-child"><a href=".././">Home</a></li>
+						<li data-filtertext="Web Development"><a href="../webdev/" data-ajax="false">Web Development</a></li>
+						<li data-filtertext="Android Development Mobile Apps Java"><a href="../androiddev/" data-ajax="false">Android Development</a></li>
+						<li data-filtertext="Social Media"><a href="../socialmedia/" data-ajax="false">Social Media</a></li>
+						<li data-filtertext="Resume Experience Projects"><a href="../resume/" data-ajax="false">Resume</a></li>
+						<li data-filtertext="Contact"><a href="../contact/" data-ajax="false">Contact</a></li>
+					</ul>
 				</div>
-				<a href="#ui-page-top" class="jqm-deeplink ui-icon-carat-u ui-alt-icon">Top</a>
-			</div>
-			<!-- /main content -->
-			<!-- Menu -->
-			<div data-role="panel" class="jqm-navmenu-panel" data-position="left" data-display="overlay" data-theme="a">
-				<ul class="jqm-list ui-alt-icon ui-nodisc-icon">
-					<li data-filtertext="KMS Ken Kenneth Solans home homepage" data-icon="home" class="ui-first-child"><a href=".././">Home</a></li>
-					<li data-filtertext="Web Development"><a href="../webdev/" data-ajax="false">Web Development</a></li>
-					<li data-filtertext="Android Development Mobile Apps Java"><a href="../androiddev/" data-ajax="false">Android Development</a></li>
-					<li data-filtertext="Social Media"><a href="../socialmedia/" data-ajax="false">Social Media</a></li>
-					<li data-filtertext="Resume Experience Projects"><a href="../resume/" data-ajax="false">Resume</a></li>
-					<li data-filtertext="Contact"><a href="../contact/" data-ajax="false">Contact</a></li>
-				</ul>
-			</div>
-			<!-- /Menu -->
-			<!-- Footer -->
-			<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
-				<p>Kenneth M Solans</p>
-				<p>Copyright 2014 Kenneth M Solans</p>
-				</div><!-- /Footer -->
-			</body>
-		</html>
+				<!-- /Menu -->
+				<!-- Footer -->
+				<div data-role="footer" data-position="fixed" data-tap-toggle="false" class="jqm-footer">
+					<p>Kenneth M Solans</p>
+					<p>Copyright 2014 Kenneth M Solans</p>
+					</div><!-- /Footer -->
+				</body>
+			</html>
